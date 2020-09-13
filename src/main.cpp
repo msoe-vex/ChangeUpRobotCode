08ffdda345029dfb6e4abe486b836df58288ab8f
Andrew Kempen <Wishbonea@users.noreply.github.com>
2020-09-12 21:15:20 -0500

    Removed two project structure

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,8 @@ void on_center_button() {
     if (pressed) {
         pros::lcd::set_text(2, "I was pressed!");
     } else {
-        pros::lcd::clear_line(2);
+        pros::lcd::clear_line(5);
+        
     }
 }
 
