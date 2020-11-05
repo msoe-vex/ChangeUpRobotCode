423821d40272d90a763c2fea78addee53f351829
Dylan-Powers <72242179+Dylan-Powers@users.noreply.github.com>
2020-11-04 19:07:24 -0600

    Added framework and adjusted definition names to leftDriveSensorNode.h and leftDriveSensorNode.cpp

--- include/inputNodes/leftDriveSensorNode.h
+++ include/inputNodes/leftDriveSensorNode.h
@@ -1,6 +1,25 @@
 #ifndef _LEFT_DRIVE_SENSOR_NODE_H_
 #define _LEFT_DRIVE_SENSOR_NODE_H_
 
-#include "..\api.h"
+#include "api.h"
+#include "constants.h"
+#include "ros_lib/ros.h"
+#include "ros_lib/std_msgs/Int16.h"
+#include "ros_lib/V5Publisher.h"
+
+class LeftDriveSensorNode : public V5Publisher<std_msgs::Int16> {
+private:
+    ros::NodeHandle* _handle;
+    std_msgs::Int16* _encoder_pos;
+    std_msgs::Int16* _encoder_vel;
+public:
+    LeftDriveSensorNode();
+
+    void setup();
+
+    void publish();
+
+    ~LeftDriveSensorNode();
+};
 
 #endif
